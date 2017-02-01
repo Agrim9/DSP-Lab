1a4a8a2e6936e35998ae3d393f7949e7244a0a61
Agrim9 <agrim9gupta@gmail.com>
2017-02-01 17:33:34 +0530

    Lab 3

---
+++ Lab_3_Projects/Project_1/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -l"/home/agrim/Downloads/C5515_Support_Files/C5515_Lib/usbstk5515bsl.lib" -l"/opt/ti/ccsv7/tools/compiler/c5500_4.4.1/lib/rts55x.lib"
+
---
+++ Lab_3_Projects/Project_2/Debug/main.d
@@ -0,0 +1,17 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: /opt/ti/ccsv7/tools/compiler/c5500_4.4.1/include/stdio.h
+main.obj: /opt/ti/ccsv7/tools/compiler/c5500_4.4.1/include/linkage.h
+main.obj: /opt/ti/ccsv7/tools/compiler/c5500_4.4.1/include/stdarg.h
+main.obj: /home/agrim/Downloads/C5515_Support_Files/C5515_Includes/usbstk5515.h
+main.obj: /home/agrim/Downloads/C5515_Support_Files/C5515_Includes/usbstk5515_gpio.h
+main.obj: /home/agrim/Downloads/C5515_Support_Files/C5515_Includes/usbstk5515_i2c.h
+
+../main.c: 
+/opt/ti/ccsv7/tools/compiler/c5500_4.4.1/include/stdio.h: 
+/opt/ti/ccsv7/tools/compiler/c5500_4.4.1/include/linkage.h: 
+/opt/ti/ccsv7/tools/compiler/c5500_4.4.1/include/stdarg.h: 
+/home/agrim/Downloads/C5515_Support_Files/C5515_Includes/usbstk5515.h: 
+/home/agrim/Downloads/C5515_Support_Files/C5515_Includes/usbstk5515_gpio.h: 
+/home/agrim/Downloads/C5515_Support_Files/C5515_Includes/usbstk5515_i2c.h: 
